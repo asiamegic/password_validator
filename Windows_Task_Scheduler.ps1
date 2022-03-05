890af70c81fe29a9321371ef665e0f926cb27827
asiamegic <85096533+asiamegic@users.noreply.github.com>
2022-03-05 18:56:10 +0200

    Delete Windows_Task_Scheduler.ps1

--- Windows_Task_Scheduler.ps1
+++
@@ -1,7 +0,0 @@
-$time = New-JobTrigger -Daily -At 9:00AM
-
-$creds = Get-Credential Artem
-
-$rights = New-ScheduledJobOption -RunElevated
-
-Register-ScheduledJob -Name Start_notepad -FilePath C:\Users\Artem\Desktop\run_notepad.ps1 -Trigger $time -Credential $creds -ScheduledJobOption $rights
